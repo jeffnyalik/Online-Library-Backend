88e7b484da93e2104126edff3e03ee0e80b531a7
Jeff Nyalik <jeffnyak@gmail.com>
2021-11-20 16:41:54 +0300

    Prod env config

--- core/settings/prod.py
+++ core/settings/prod.py
@@ -6,14 +6,21 @@ load_dotenv()
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': os.getenv('NAME'),
+#         'USER': os.getenv('USER'),
+#         'PASSWORD': os.getenv('PASSWORD'),
+#         'HOST':os.getenv('HOST'),
+#         'PORT':os.getenv('PORT')
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv('NAME'),
-        'USER': os.getenv('USER'),
-        'PASSWORD': os.getenv('PASSWORD'),
-        'HOST':os.getenv('HOST'),
-        'PORT':os.getenv('PORT')
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
--- core/wsgi.py
+++ core/wsgi.py
@@ -11,6 +11,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings.dev')
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings.dev')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings.prod')
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings.stage')
+
 
 application = get_wsgi_application()
