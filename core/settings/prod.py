507e8a1fa3809772ed7be510bc256af75ef3e88f
Jeff Nyalik <jeffnyak@gmail.com>
2021-11-20 17:31:25 +0300

    Keys

--- core/settings/prod.py
+++ core/settings/prod.py
@@ -1,36 +1,21 @@
 from .base import *
 import os
 from dotenv import load_dotenv
+import django_heroku
 
 load_dotenv()
-
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': os.getenv('NAME'),
-#         'USER': os.getenv('USER'),
-#         'PASSWORD': os.getenv('PASSWORD'),
-#         'HOST':os.getenv('HOST'),
-#         'PORT':os.getenv('PORT')
-#     }
-# }
 
 DEBUG=True
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 SECRET_KEY = os.getenv('SECRET_KEY', '2534a4dde10e2dbdebdb2675b6ac96aeb54409854db0e3a05424a33067a4')
 
 ALLOWED_HOSTS = ['online-library1990.herokuapp.com']
-
-import dj_database_url
-prod_db = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
+django_heroku.settings(locals)
